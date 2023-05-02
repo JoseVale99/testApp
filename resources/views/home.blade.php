5c0d9ffa33e9f4bec751f2b45db34d294e34a503
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2023-05-02 15:50:53 -0600

    add: menu

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Menu') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        {{-- menu de cruds category --}}
+
+                        <div class="d-flex flex-column">
+                            <div class="p-2"><a href="{{ route('categories.index') }}" class="btn btn-dark">Crud de
+                                    categorias</a></div>
+
+                            <div class="p-2"><a href="{{ route('barcodes.index') }}" class="btn btn-primary">Crud de
+                                    Códigos de barra</a></div>
+
+                            <div class="p-2"><a href="{{ route('prices.index') }}" class="btn btn-success">Crud de
+                                    precios</a></div>
+
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
