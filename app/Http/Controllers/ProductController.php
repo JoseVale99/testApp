44bcb14450cd68f8fb0b21cafc87201d21f9d4e0
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2023-05-02 17:51:27 -0600

    add: nueva release

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -49,4 +49,41 @@ class ProductController extends Controller
         return redirect()->route('products.index')
             ->with('success', 'Producto creado con éxito');
     }
+
+    public function show(Product $product)
+    {
+        return view('products.show', compact('product'));
+    }
+
+    public function edit(Product $product)
+    {
+        return view('products.edit', compact('product'));
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $request->validate([
+            'codigo' => 'required|unique:products|regex:/^[0-9]+$/|max:20',
+            'descripcion' => 'required|unique:products|max:60',
+            'category_id' => 'required|exists:categories,id',
+            'barcode' => 'required|array|min:1',
+            // 'barcode.*' => 'required|exists:barcodes,id',
+            'precios' => 'required|array|min:1',
+            // 'precios.*' => 'required|exists:prices,id',
+            'estado' => 'required',
+        ]);
+
+        $product->update($request->all());
+
+        return redirect()->route('products.index')
+            ->with('success', 'Producto actualizado con éxito');
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return redirect()->route('products.index')
+            ->with('success', 'Producto eliminado con éxito');
+    }
 }
