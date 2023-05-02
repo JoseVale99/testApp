e8d45ffa8aa3e266d024fdecaddad9915e1955c1
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2023-05-02 13:43:11 -0600

    add: seeder user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +15,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        // create user
+
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin',
+            'password' => bcrypt('123456789'),
+        ]);
     }
 }
